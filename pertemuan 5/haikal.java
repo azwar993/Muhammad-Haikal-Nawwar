397f3cece31731253597779bf6982ab92d380ea3
azwar993 <azwarsky993@gmail.com>
2026-05-06 14:16:01 +0700

    Delete pertemuan 5 directory

--- pertemuan 5/haikal.java
+++
@@ -1,9 +0,0 @@
-public class haikal {
-    public static void main(String [] args){
-        int nilai = 100;
-        if(nilai > 90){
-            System.out.println("Selamat Anda Lulus");
-            System.out.println("Dengan Nilai " + nilai + "!");
-        }
-    }
-}
